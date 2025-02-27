d1283be38d50f593609d017619c4767db39c56ea
Druva2804 <druva.bhagavatula@gmail.com>
2025-02-27 22:13:16 +0530

    Update

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app=express();
 const mongoose=require("mongoose");
 const Listing=require("./models/listing.js");
 const path=require("path");
+const methodOverride=require("method-override");
 
 
 
@@ -25,6 +26,7 @@ app.get("/",(req,res)=>{
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
 app.use(express.urlencoded({extended:true}));
+app.use(methodOverride("_method"));
 
 // app.get("/testlisting",async(req,res)=>{
 //     let sampleListing=new Listing({
@@ -78,6 +80,13 @@ app.get("/listings/:id/edit",async(req,res)=>{
     let {id}=req.params;
     const listing=await Listing.findById(id);
     res.render("listings/edit.ejs",{listing});
+});
+
+//Update Route
+app.put("/listings/:id",async(req,res)=>{
+    let {id}=req.params;
+    await Listing.findByIdAndUpdate(id,{...req.body});
+    res.redirect("/listings");
 });
 
 app.listen(8080,()=>{
